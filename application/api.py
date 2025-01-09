58b45778d07601b97c7fd85bc3d2b41eea833909
KUNKUN2121 <kunkun7532@gmail.com>
2025-01-09 15:31:32 +0900

    いいね機能の実装

--- application/api.py
+++ application/api.py
@@ -1,9 +1,9 @@
 from model.blog import Blog
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from model.comment import Comment
 
-api_bp = Blueprint('api',__name__)
 
+api_bp = Blueprint('api',__name__)
 
 @api_bp.route('/comment',methods=['POST'])
 def comment():
@@ -18,16 +18,17 @@ def comment():
         return 500
 
 # いいね処理
-@api_bp.route('/like/<int:blog_id>',methods=['POST'])
-def like(blog_id):
+@api_bp.route('/like', methods=['POST'])
+def like():
     try:
+        blog_id = request.form['blog_id']
         # IDが一致するブログを取得
         blog = Blog.get_by_id(blog_id)
         blog.likes += 1
         blog.save()
-        return 200
+        return jsonify(status="success", code=200)
     except Blog.DoesNotExist:
-        return 404
+        return jsonify(status="fail", code=404)
 
 # いいね解除処理
 @api_bp.route('/unlike/<int:blog_id>',methods=['POST'])
--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from flask import Flask, request, jsonify
 from model import init_db
 from application.create import create_bp
 from application.view import *
+from application.api import api_bp
 
 app = Flask(__name__)
 app.register_blueprint(create_bp)
 app.register_blueprint(view_bp)
+app.register_blueprint(api_bp)
 
 init_db()
 
 # home
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
