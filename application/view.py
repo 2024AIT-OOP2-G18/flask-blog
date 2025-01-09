16b16d2cf07f672391e21975104e1502e08624e2
k23022 <k23022kk@aitech.ac.jp>
2025-01-09 15:07:23 +0900

    view.pyとview_detail.htmlを繋げました

--- application/view.py
+++ application/view.py
@@ -1,5 +1,7 @@
 from model.blog import Blog
+from model.comment import Comment
 from flask import Blueprint, render_template, request, redirect, url_for
+from datetime import datetime
 
 view_bp = Blueprint('view',__name__)
 
@@ -16,7 +18,9 @@ def blog_detail(blog_id):
     try:
         # IDが一致するブログを取得
         blog = Blog.get_by_id(blog_id)
-        return "test"
+
+        comments = blog.comments
+        return render_template('view_detail.html',blog=blog, comments=comments)
         # return f"タイトル：{blog.title}<br>内容：{blog.content}<br>記載日時：{blog.created_at}"
     except Blog.DoesNotExist:
         return f"ID {blog_id} のブログは存在しません。"
